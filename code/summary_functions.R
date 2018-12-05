8ce79ed3ec7d44f3ad745fd88b4e9eab6affcd0a
simingz <michellezhao.student@gmail.com>
2018-12-05 15:05:14 -0600

    de_anlysis

---
+++ code/summary_functions.R
@@ -0,0 +1,10 @@
+
+summ_pvalues <- function(pvalues){
+  source('code/qq-plot.R')
+  library(gridExtra)
+  # p values distribution histogram
+  plot1 <- histogram(pvalues,col='grey',type="count",xlim=c(0,1),breaks=100, main= "p value distribution")
+  # pvalues qq-plot
+  plot2 <- qqunif.plot(pvalues, main="p value qq-plot")
+  grid.arrange(plot1,plot2, ncol=2)
+}
